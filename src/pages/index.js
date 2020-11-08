8d610bb019ce44b9194adc081d4648c7e1cfa960
Gabby Pelton <gabriellapelton@gmail.com>
2020-11-07 20:46:15 -0600

    Add vision, mission, and aims to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,60 +1,68 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import get from 'lodash/get'
-import { Helmet } from 'react-helmet'
-import Hero from '../components/hero'
-import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
+import React from "react";
+import { graphql } from "gatsby";
+import get from "lodash/get";
+import { Helmet } from "react-helmet";
+import Hero from "../components/hero";
+import Layout from "../components/layout";
 
 class RootIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
+    const siteTitle = get(this, "props.data.site.siteMetadata.title");
+    const sections = get(this, "props.data.allContentfulSection.nodes");
+    const [author] = get(this, "props.data.allContentfulPerson.edges");
 
     return (
       <Layout location={this.props.location}>
-        <div style={{ background: '#fff' }}>
+        <div style={{ background: "#fff" }}>
           <Helmet title={siteTitle} />
           <Hero data={author.node} />
-          <div className="wrapper">
-            <h2 className="section-headline">Recent articles</h2>
-            <ul className="article-list">
-              {posts.map(({ node }) => {
-                return (
-                  <li key={node.slug}>
-                    <ArticlePreview article={node} />
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+          {sections.map((section, index) => {
+            if (index % 2 === 0) {
+              return (
+                <div style={{ padding: "0 2% 1rem 2%" }}>
+                  <h1>{section.title}</h1>
+                  {section.body.childMarkdownRemark.htmlAst.children.map(
+                    (i) => (
+                      <RecursiveRender key={i.id} data={i} />
+                    )
+                  )}
+                </div>
+              );
+            }
+            return null;
+          })}
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default RootIndex
+const RecursiveRender = ({ data }) => {
+  if (data.type === "text") return data.value;
+  const Element = () =>
+    React.createElement(
+      data.tagName,
+      {},
+      data.children.map((child) => (
+        <RecursiveRender key={child.id} data={child} />
+      ))
+    );
+  return <Element />;
+};
+
+export default RootIndex;
 
 export const pageQuery = graphql`
   query HomeQuery {
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      edges {
-        node {
-          title
-          slug
-          publishDate(formatString: "MMMM Do, YYYY")
-          tags
-          heroImage {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
-          description {
-            childMarkdownRemark {
-              html
-            }
+    allContentfulSection(sort: { fields: createdAt, order: ASC }) {
+      nodes {
+        id
+        title
+        body {
+          id
+          childMarkdownRemark {
+            id
+            htmlAst
           }
         }
       }
@@ -83,4 +91,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
