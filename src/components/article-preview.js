df37e49a0145b0e4f7269aff4e74f09f0bdd79af
Gabby Pelton <gabriellapelton@gmail.com>
2021-01-03 15:57:19 -0600

    Article preview image link to article

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -6,7 +6,9 @@ import styles from "./article-preview.module.css";
 
 export default ({ article }) => (
   <div className={styles.preview}>
-    <Img alt={article.heroImage.title} fluid={article.heroImage.fluid} />
+    <Link to={`/blog/${article.slug}`}>
+      <Img alt={article.heroImage.title} fluid={article.heroImage.fluid} />
+    </Link>
     <h3 className={styles.previewTitle}>
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
     </h3>
