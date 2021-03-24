2fae1500964ef452fd53c0cfac38cb089434afe6
Gabby Pelton <gabriellapelton@gmail.com>
2021-03-24 05:53:54 -0500

    Add donate link to navigation tabs

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -15,7 +15,10 @@ export default () => (
         <Link to="/contact/">Contact</Link>
       </li>
       <li className={styles.navigationItem}>
-        <a href="https://forms.gle/ZReCP2RDc6mpeNsr6">Get Involved</a>
+        <a href="https://forms.gle/ZReCP2RDc6mpeNsr6">Join</a>
+      </li>
+      <li className={styles.navigationItem}>
+        <a href="https://opencollective.com/cooperation-denton">Donate</a>
       </li>
     </ul>
   </nav>
